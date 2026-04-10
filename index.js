fb24185ba382dcac49439f8636657bdd615eb2e4
juanjo-xmd <est.jjvalenciaa35@manuelabeltransoacha.edu.co>
2026-04-09 22:32:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 
 const app = express();
-
-app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("Mongo conectado"))
-.catch(err => console.log(err));
+.then(() => console.log("Mongo conectado"));
 
-app.use('/users', require('./routes/userRoutes'));
+app.use('/users', require('./routes/userRoutes')); // 👈 ESTO
 
 const PORT = process.env.PORT || 3000;
 
